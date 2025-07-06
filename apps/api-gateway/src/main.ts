faf5f143b0b28dfdee600f1cc740e3b63b9e8dcd
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-07-07 00:36:17 +0530

    api-gateway service proxy passed

---
+++ apps/api-gateway/src/main.ts
@@ -0,0 +1,56 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import express from 'express';
+import * as path from 'path';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
+import porxy from 'express-http-proxy';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import swagger from 'swagger-ui-express';
+import axios from 'axios';
+
+
+const app = express();
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }
+));
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+  standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: true, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req: any) => req.ip,
+}));
+app.set('trust proxy', 1);
+
+
+
+
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/gateway-health', (req, res) => {
+  res.send({ message: 'Welcome to api-gateway!' });
+});
+
+app.use('/',porxy("http://localhost:6001"));
+
+const port = process.env.PORT || 8081;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
