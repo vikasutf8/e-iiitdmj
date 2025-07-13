972ed11783f65068d5e01a25214cdba2523b7978
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-07-13 23:29:35 +0530

    template and smtp

--- apps/auth-service/src/controllers/auth.controller.ts
+++ apps/auth-service/src/controllers/auth.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
-import { checkOtpRestrictions, trackOtpRequest, validateRegistrationData } from '../utils/auth.helper';
+import { checkOtpRestrictions, sentOtp, trackOtpRequest, validateRegistrationData } from '../utils/auth.helper';
 import prisma from '../../../../packages/libs/prisma';
 import { ValidationError } from '../../../../packages/error-handler';
 
 //Register a new user
 
 export const  userRegistration = async (req:Request,res:Response,next:NextFunction)=>{
-    validateRegistrationData(req.body,'user');
+   try {
+     validateRegistrationData(req.body,'user');
     const {email,name} = req.body;
 
     const existingUser = await prisma.users.findUnique({
@@ -23,5 +24,15 @@ export const  userRegistration = async (req:Request,res:Response,next:NextFuncti
 
     await checkOtpRestrictions(email,next)
     await trackOtpRequest(email,next)
+    await sentOtp(email,name,"user-activation-mail")
 
+
+   res.status(200).json({
+        status:"success",
+        message:"Otp sent successfully | Verify your account",
+    });
+   } catch (error) {
+        console.log("user registration error",error);
+        return next(error);
+   }
 };
