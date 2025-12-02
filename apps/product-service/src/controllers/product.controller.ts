2343a94569aafda6d469e331e48fe20a45b25085
vikasutf8 <21bcs241@iiitdmj.ac.in>
2025-12-02 22:49:16 +0530

    Feat: discount code controller and route defined

--- apps/product-service/src/controllers/product.controller.ts
+++ apps/product-service/src/controllers/product.controller.ts
@@ -1,16 +1,135 @@
 //get products category
-import { Router, Request, Response } from "express";
-import prisma from "../../../../packages/libs/prisma";
-
+import { Router, Request, Response, NextFunction } from 'express';
+import prisma from '../../../../packages/libs/prisma';
+import { NotFoundError, ValidationError } from '@packages/error-handler';
 
 export const getCategories = async (req: Request, res: Response) => {
   try {
     const config = await prisma.site_config.findFirst();
-    if(!config){
-      return res.status(404).json({ message: "Category not found" });
+    if (!config) {
+      return res.status(404).json({ message: 'Category not found' });
     }
     res.status(200).json(config.categories);
   } catch (error) {
-    res.status(500).json({ message: "Internal Server Error" });
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
+
+//create discount code
+
+export const createDiscountCode = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { public_name, discountType, discountValue, discountCode } = req.body;
+    // if(!public_name || !discountType || !discountValue || !discountCode || !sellerId){
+    //   return next(new ValidationError("Please provide all the required fields"));
+    // }
+
+    const isDiscountCodeExists = await prisma.discount_codes.findUnique({
+      where: {
+        discountCode,
+      },
+    });
+
+    if (isDiscountCodeExists) {
+      return next(
+        new ValidationError(
+          'Discount code already exists !! Please try with different discount code'
+        )
+      );
+    }
+
+    const discount_code = await prisma.discount_codes.create({
+      data: {
+        public_name,
+        discountType,
+        discountValue: parseFloat(discountValue),
+        discountCode,
+        sellerId: req.seller.id,
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      success: true,
+      message: 'Discount code created successfully',
+      discount_code,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+//get discount code
+export const getDiscountCode = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const discount_codes = await prisma.discount_codes.findMany({
+      where: {
+        sellerId: req.seller.id,
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      success: true,
+      message: 'Discount codes found successfully',
+      discount_codes,
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+//delete discount code
+export const deleteDiscountCode = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { id } = req.body;
+    const sellerId = req.seller.id;
+    //ownership check
+    const discount_code = await prisma.discount_codes.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        sellerId: true,
+      },
+    });
+
+    if (!discount_code) {
+      return next(new NotFoundError('Discount code not found'));
+    }
+
+    if (discount_code.sellerId !== sellerId) {
+      return next(new ValidationError('Unauthorized'));
+    }
+
+    await prisma.discount_codes.delete({
+      where: {
+        id,
+      },
+    });
+
+    res.status(200).json({
+      status: 'success',
+      success: true,
+      message: 'Discount code deleted successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 };
--- apps/product-service/src/routes/product.route.ts
+++ apps/product-service/src/routes/product.route.ts
@@ -1,5 +1,6 @@
 import express,{ Router } from "express";
-import { getCategories } from "../controllers/product.controller";
+import { createDiscountCode, deleteDiscountCode, getCategories, getDiscountCode } from "../controllers/product.controller";
+import { isAuthenticated } from "@packages/middleware/isAuthenticated";
 
 
 const router:Router = express.Router();
@@ -7,5 +8,9 @@ const router:Router = express.Router();
 
 router.get("/get-categories",getCategories);
 
+// create discount code
+router.post("/create-discount-code",isAuthenticated,createDiscountCode);
+router.get("/get-discount-code",isAuthenticated,getDiscountCode);
+router.delete("/delete-discount-code",isAuthenticated,deleteDiscountCode);
 
 export default router;
